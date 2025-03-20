b0d0a02902c1d05f5b6731fb454b04c4a62a1165
Hussein Rizk <husseinrizk117@gmail.com>
2025-03-20 06:20:06 +0200

    build App Bar

--- lib/main.dart
+++ lib/main.dart
@@ -13,55 +13,6 @@ void main() {
         )),
         backgroundColor: Colors.red,
       ),
-      body: dicee(),
     ),
   ));
-}
-
-class dicee extends StatefulWidget {
-  const dicee({super.key});
-
-  @override
-  State<dicee> createState() => _diceeState();
-}
-
-class _diceeState extends State<dicee> {
-  int leftdiceenum = 6;
-  int rightdiceenum = 6;
-  Function? randomnum() {
-    setState(() {
-      leftdiceenum = Random().nextInt(6) + 1;
-      rightdiceenum = Random().nextInt(6) + 1;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Row(
-        children: [
-          Expanded(
-            child: TextButton(
-              onPressed: () {
-                randomnum();
-              },
-              child: Image.asset(
-                'images/dice$leftdiceenum.png',
-              ),
-            ),
-          ),
-          Expanded(
-            child: TextButton(
-              onPressed: () {
-                randomnum();
-              },
-              child: Image.asset(
-                'images/dice$rightdiceenum.png',
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
 }
